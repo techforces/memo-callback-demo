44b1d1aaf5479d86d8f68d98b684510572817348
khurshid <ernazarovkh@gmail.com>
2024-09-23 23:09:08 +0200

    feat: add useMemo demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState, useMemo } from "react";
 import "./App.css";
 
 function List({ getArray, label }) {
@@ -16,6 +16,48 @@ function List({ getArray, label }) {
   );
 }
 
+function SlowButton({ number }) {
+  const [theme, setTheme] = useState("dark");
+  const SlowDoubleNum = () => {
+    return doubleNum(number);
+  };
+  return (
+    <button
+      onClick={() => setTheme(theme == "dark" ? "light" : "dark")}
+      style={{
+        backgroundColor: theme == "dark" ? "#333" : "#fff",
+        color: theme == "dark" ? "#fff" : "#333",
+      }}
+    >
+      Slow switch: {SlowDoubleNum()}
+    </button>
+  );
+}
+
+function FastButton({ number }) {
+  const FastDoubleNum = useMemo(() => {
+    return doubleNum(number);
+  }, [number]);
+
+  const [theme, setTheme] = useState("dark");
+  return (
+    <button
+      onClick={() => setTheme(theme == "dark" ? "light" : "dark")}
+      style={{
+        backgroundColor: theme == "dark" ? "#333" : "#fff",
+        color: theme == "dark" ? "#fff" : "#333",
+      }}
+    >
+      Fast switch: {FastDoubleNum}
+    </button>
+  );
+}
+
+const doubleNum = (num) => {
+  for (let i = 0; i < 1000000000; i++) {}
+  return num * 2;
+};
+
 function App() {
   const [count, setCount] = useState(0);
   const [theme, setTheme] = useState("dark");
@@ -30,22 +72,38 @@ function App() {
 
   return (
     <>
-      <h1>useCallback() demo</h1>
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button onClick={() => setTheme(theme == "dark" ? "light" : "dark")}>
-          Trigger
-        </button>
+        <h1>useCallback() demo</h1>
+        <div>
+          <button onClick={() => setTheme(theme == "dark" ? "light" : "dark")}>
+            Trigger
+          </button>
 
-        <List getArray={getArray} label="Array" />
-        <List getArray={getArray2} label="Reversed array" />
-        <p>
-          Trigger should update only the "Array", the reversed one should stay
-          unchanged. UseCallback will have no side effects on Reversed Array.
-          Check console to see the difference
-        </p>
+          <List getArray={getArray} label="Array" />
+          <List getArray={getArray2} label="Reversed array" />
+          <p>
+            Trigger should update only the "Array", the reversed one should stay
+            unchanged. UseCallback will have no side effects on Reversed Array.
+            Check console to see the difference
+          </p>
+        </div>
+      </div>
+
+      <div className="card">
+        <h1>useMemo() demo</h1>
+
+        <div>
+          <SlowButton number={count} />
+          <FastButton number={count} />
+
+          <p>
+            Slow switch re-initializes expensive function while fast caches the
+            result with useMemo.
+          </p>
+        </div>
       </div>
     </>
   );
